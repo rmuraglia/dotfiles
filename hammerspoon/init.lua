1cf24f8b46cc349e3dc053ddf74dfef3e131bed5
Ryan Muraglia <rmuraglia@gmail.com>
2022-06-17 17:02:16 -0400

    add hs middle click drag scroll

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -331,3 +331,66 @@ hs.hotkey.bind(lcag, 'K', snapWindow(100, 'center'))
 hs.loadSpoon('Emojis')
 
 spoon.Emojis:bindHotkeys({toggle = {{"cmd", "alt"}, 'e'}})
+
+------------
+-- MIDDLE CLICK DRAG SCROLL
+------------
+-- tweaked from: https://superuser.com/a/1351638
+
+-- HANDLE SCROLLING WITH MOUSE BUTTON PRESSED
+local scrollMouseButton = 3
+local deferred = false
+
+overrideOtherMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
+    -- print("down")
+    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+    if scrollMouseButton == pressedMouseButton 
+        then 
+            deferred = true
+            return true
+        end
+end)
+
+overrideOtherMouseUp = hs.eventtap.new({ hs.eventtap.event.types.otherMouseUp }, function(e)
+    -- print("up")
+    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+    if scrollMouseButton == pressedMouseButton 
+        then 
+            if (deferred) then
+                overrideOtherMouseDown:stop()
+                overrideOtherMouseUp:stop()
+                hs.eventtap.otherClick(e:location(), pressedMouseButton)
+                overrideOtherMouseDown:start()
+                overrideOtherMouseUp:start()
+                return true
+            end
+            return false
+        end
+        return false
+end)
+
+local oldmousepos = {}
+local scrollmult = -4   -- negative multiplier scrolls in same direction as your finger movement
+
+dragOtherToScroll = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDragged }, function(e)
+    local pressedMouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+    -- print ("pressed mouse " .. pressedMouseButton)
+    if scrollMouseButton == pressedMouseButton 
+        then 
+            -- print("scroll");
+            deferred = false
+            oldmousepos = hs.mouse.getAbsolutePosition()    
+            local dx = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaX'])
+            local dy = e:getProperty(hs.eventtap.event.properties['mouseEventDeltaY'])
+            local scroll = hs.eventtap.event.newScrollEvent({dx * scrollmult, dy * scrollmult},{},'pixel')
+            -- put the mouse back
+            hs.mouse.setAbsolutePosition(oldmousepos)
+            return true, {scroll}
+        else 
+            return false, {}
+        end 
+end)
+
+overrideOtherMouseDown:start()
+overrideOtherMouseUp:start()
+dragOtherToScroll:start()
