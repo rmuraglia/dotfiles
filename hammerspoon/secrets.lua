59b9b0722b8788948e601d3bd967e51d57ccb492
Ryan Muraglia <rmuraglia@gmail.com>
2024-08-16 15:17:32 -0400

    add slack to markdown super paste

--- hammerspoon/secrets.lua
+++ hammerspoon/secrets.lua
@@ -4,6 +4,7 @@ function superPaste()
     -- common enhancements to paste for markdown, like:
     -- 1) if you copied a jira URL for a ticket (PROJECT-num), then paste it formatted like [PROJECT-num](URL)
     -- 2) if you copied a github PR URL, then paste it formatted like [REPO#ddd](URL)
+    -- 3) if you copied a slack URL, then paste it as [slack](URL)
 
     local copiedText = hs.pasteboard.getContents()
 
@@ -12,11 +13,16 @@ function superPaste()
 
     local gitPattern = '([%w-]+)/pull/(%d+)'
     local gitMatchRepo, gitMatchNum = string.match(copiedText, gitPattern)
+    
+    local slackPattern = 'slack.com/archives/'
+    local slackMatch = string.match(copiedText, slackPattern)
 
     if jiraMatch ~= nil then
         ret = '[' .. jiraMatch .. '](' .. copiedText .. ')'
     elseif gitMatchRepo ~= nil and gitMatchNum ~= nil then
         ret = '[' .. gitMatchRepo .. '#' .. gitMatchNum .. '](' .. copiedText .. ')'
+    elseif slackMatch ~= nil then
+        ret = '[slack](' .. copiedText .. ')'
     else
         ret = copiedText
     end
